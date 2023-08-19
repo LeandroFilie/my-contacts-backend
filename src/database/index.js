6ed9643118b3178e4e62967ec9ab56a393fda453
LeandroFilie <leandro.gfilie@gmail.com>
2023-08-19 19:49:35 -0300

    chore: vercel deploy

--- src/database/index.js
+++ src/database/index.js
@@ -1,16 +1,12 @@
-const { Client } = require('pg');
+const { Pool } = require('pg');
 
-const client = new Client({
-  host: process.env.DB_HOST,
-  port: 5432,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
+const pool = new Pool({
+  connectionString: `${process.env.POSTGRES_URL}?sslmode=require`,
 });
 
-client.connect();
+pool.connect();
 
 exports.query = async (query, values) => {
-  const { rows } = await client.query(query, values);
+  const { rows } = await pool.query(query, values);
   return rows;
 };
